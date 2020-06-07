27e202456c74bdc8f8f33d3917109eccdde11466
Miguel Suez Xve Penteado <sp-miguel@pernilongo>
2020-06-07 02:42:00 -0300

    1

--- bancos/README.md
+++
@@ -1,2 +0,0 @@
-# Bancos
-## Introdução
--- ferramentas/windows/pid.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-
-set titl=RANDOMTITLE%RANDOM%
-title %titl%
-for /f tokens^=3^ delims^=^" %%a in ('tasklist /nh /v /fi "WINDOWTITLE eq %titl%" /fo csv') do (
-set PID=%%a
-)
-echo %PID% >> %TMP%\processo.pid
